326aed155cb50c02ebc39c386ebff5c19b8b93c3
Arto Bendiken <arto@conreality.org>
2019-08-08 20:02:46 +0300

    Ensured that PulseSource is Disposable.

--- src/main/java/org/conreality/sdk/android/PulseSource.java
+++ src/main/java/org/conreality/sdk/android/PulseSource.java
@@ -5,19 +5,34 @@ package org.conreality.sdk.android;
 import android.content.Context;
 import android.util.Log;
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import io.reactivex.ObservableEmitter;
 import io.reactivex.ObservableOnSubscribe;
+import io.reactivex.disposables.Disposable;
 import java.util.LinkedHashSet;
 import java.util.Set;
 
 /** PulseSource */
-public abstract class PulseSource implements ObservableOnSubscribe<Float> {
+public abstract class PulseSource implements Disposable, ObservableOnSubscribe<Float> {
   protected static final String TAG = "ConrealitySDK";
 
-  protected Set<ObservableEmitter<Float>> emitters = new LinkedHashSet<ObservableEmitter<Float>>(1);
+  protected @Nullable Set<ObservableEmitter<Float>> emitters =
+      new LinkedHashSet<ObservableEmitter<Float>>(1);
 
   public PulseSource(final @NonNull Context context) {
     assert(context != null);
+  }
+
+  /** Implements io.reactivex.disposables.Disposable#isDisposed(). */
+  @Override
+  public boolean isDisposed() {
+    return this.emitters == null;
+  }
+
+  /** Implements io.reactivex.disposables.Disposable#dispose(). */
+  @Override
+  public void dispose() {
+    this.emitters = null;
   }
 
   /** Implements io.reactivex.ObservableOnSubscribe#subscribe(). */
@@ -25,14 +40,18 @@ public abstract class PulseSource implements ObservableOnSubscribe<Float> {
   public void subscribe(@NonNull ObservableEmitter<Float> emitter) throws Exception {
     assert(emitter != null);
     Log.d(TAG, "PulseSource.subscribe");
-    this.emitters.add(emitter);
+    if (this.emitters != null) {
+      this.emitters.add(emitter);
+    }
   }
 
   /** Emits a heart-rate measurement to all observers. */
   protected void emit(final float measurement) {
-    for (final ObservableEmitter<Float> emitter : this.emitters) {
-      if (!emitter.isDisposed()) {
-        emitter.onNext(measurement);
+    if (this.emitters != null) {
+      for (final ObservableEmitter<Float> emitter : this.emitters) {
+        if (!emitter.isDisposed()) {
+          emitter.onNext(measurement);
+        }
       }
     }
   }
