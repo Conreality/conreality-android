8110508b04fdbb08cfc35dbd0328144eb7531b81
Arto Bendiken <arto@conreality.org>
2019-08-14 16:00:56 +0300

    Imported code from the headset plugin.

--- src/main/java/org/conreality/sdk/android/HeadsetService.java
+++ src/main/java/org/conreality/sdk/android/HeadsetService.java
@@ -2,11 +2,16 @@
 
 package org.conreality.sdk.android;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothHeadset;
 import android.bluetooth.BluetoothProfile;
+import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.ServiceConnection;
+import android.media.AudioManager;
 import android.media.AudioTrack;
 import android.os.Binder;
 import android.os.Bundle;
@@ -14,6 +19,7 @@ import android.os.IBinder;
 import android.speech.tts.TextToSpeech;
 import android.speech.tts.UtteranceProgressListener;
 import android.util.Log;
+import androidx.annotation.MainThread;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import java.io.File;
@@ -24,7 +30,7 @@ import java.util.Objects;
 import java.util.UUID;
 
 /** HeadsetService */
-public final class HeadsetService extends ConrealityService {
+public final class HeadsetService extends ConrealityService implements Headset {
   private static final String TTS_ENGINE = "com.google.android.tts";
 
   /** Bind to the service, creating it if needed. */
@@ -47,11 +53,18 @@ public final class HeadsetService extends ConrealityService {
   }
 
   private final @NonNull IBinder binder = new LocalBinder();
-  private @NonNull HeadsetStatus status = new HeadsetStatus();
+  private final @NonNull HeadsetStatus status = new HeadsetStatus();
+  private @Nullable AudioManager audioManager;
+  private @Nullable BluetoothAdapter bluetoothAdapter;
   private @Nullable BluetoothHeadset bluetoothHeadset;
   private @Nullable TextToSpeech ttsEngine;
   private @Nullable Bundle ttsParams;
   private @Nullable List<String> ttsQueue = new ArrayList<String>();
+  private @Nullable AudioRecorderThread recordingThread;
+
+  public @NonNull HeadsetStatus getStatus() {
+    return this.status;
+  }
 
   /** Implements android.app.Service#onBind(). */
   @Override
@@ -61,28 +74,71 @@ public final class HeadsetService extends ConrealityService {
   }
 
   /** Implements android.app.Service#onCreate(). */
+  @SuppressWarnings("deprecation")
   @Override
   public void onCreate() {
     super.onCreate();
-    Log.i(TAG, "Created the bound service.");
+    Log.d(TAG, "Creating the headset service...");
+
+    this.audioManager = (AudioManager)this.getSystemService(Context.AUDIO_SERVICE);
+    if (this.audioManager != null) {
+      this.status.hasWiredHeadset = this.audioManager.isWiredHeadsetOn(); // deprecated
+      this.status.hasWirelessHeadset = this.audioManager.isBluetoothA2dpOn() || this.audioManager.isBluetoothScoOn(); // deprecated
+    }
+
+    this.bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+    if (this.bluetoothAdapter != null && this.bluetoothAdapter.isEnabled()) {
+      final boolean ok = this.bluetoothAdapter.getProfileProxy(this, bluetoothListener, BluetoothProfile.HEADSET);
+      if (!ok) {
+        Log.e(TAG, "Failed to connect to the Bluetooth headset service.");
+      }
+    }
+
+    this.registerReceiver(this.broadcastReceiver, new IntentFilter(AudioManager.ACTION_HEADSET_PLUG));
+    this.registerReceiver(this.broadcastReceiver, new IntentFilter(BluetoothHeadset.ACTION_CONNECTION_STATE_CHANGED));
+    this.registerReceiver(this.broadcastReceiver, new IntentFilter(AudioManager.ACTION_SCO_AUDIO_STATE_UPDATED));
+
+    //if (this.audioManager != null) {
+    //  this.audioManager.startBluetoothSco();
+    //}
+
     this.ttsEngine = new TextToSpeech(this, this.ttsInitListener, TTS_ENGINE);
     this.ttsParams = new Bundle();
+
+    Log.i(TAG, "Created the headset service.");
   }
 
   /** Implements android.app.Service#onDestroy(). */
   @Override
   public void onDestroy() {
     super.onDestroy();
-    Log.d(TAG, "Terminating the bound service...");
+    Log.d(TAG, "Terminating the headset service...");
+
+    if (this.recordingThread != null) {
+      HeadsetService.this.recordingThread.interrupt();
+      HeadsetService.this.recordingThread = null;
+    }
+
     if (this.ttsEngine != null) {
       this.ttsEngine.shutdown();
       this.ttsEngine = null;
       this.ttsParams = null;
     }
-    if (this.ttsQueue != null) {
-      this.ttsQueue = null;
+    this.ttsQueue = null;
+
+    this.unregisterReceiver(this.broadcastReceiver);
+
+    if (this.bluetoothAdapter != null) {
+      if (this.bluetoothHeadset != null) {
+        this.bluetoothAdapter.closeProfileProxy(BluetoothProfile.HEADSET, this.bluetoothHeadset);
+        this.bluetoothHeadset = null;
+      }
+      this.bluetoothAdapter = null;
     }
-    Log.i(TAG, "Terminated the bound service.");
+
+    this.audioManager = null; // should be last, used in callbacks
+
+    Log.i(TAG, "Terminated the headset service.");
   }
 
   /** Implements android.app.Service#onStartCommand(). */
@@ -122,7 +178,8 @@ public final class HeadsetService extends ConrealityService {
     return (this.ttsEngine != null) || (this.ttsQueue != null);
   }
 
-  /** Synthesizes speech from the given text message. */
+  /** Implements Headset#speak(). */
+  @Override
   public boolean speak(final @NonNull String message) {
     Objects.requireNonNull(message);
 
@@ -202,6 +259,88 @@ public final class HeadsetService extends ConrealityService {
         HeadsetService.this.bluetoothHeadset = null;
         HeadsetService.this.status.hasWirelessHeadset = false;
         //HeadsetService.this.sendStatus(); // TODO
+      }
+    }
+  };
+
+  private final @NonNull BroadcastReceiver broadcastReceiver = new BroadcastReceiver() {
+    /** Implements BroadcastReceiver#onReceive(). */
+    @MainThread
+    @Override
+    public void onReceive(final @NonNull Context context, final @NonNull Intent intent) {
+      assert(context != null);
+      assert(intent != null);
+
+      switch (intent.getAction()) {
+        case AudioManager.ACTION_HEADSET_PLUG: {
+          final int state = intent.getIntExtra("state", -1);
+          final int microphone = intent.getIntExtra("microphone", -1);
+          if (Log.isLoggable(TAG, Log.DEBUG)) {
+            final String name = intent.getStringExtra("name");
+            Log.d(TAG, String.format("Received broadcast: %s state=%d microphone=%d name=%s", intent.toString(), state, microphone, name));
+          }
+          HeadsetService.this.status.hasWiredHeadset = (state == 1);
+          HeadsetService.this.status.hasMicrophone = (microphone == 1);
+          //HeadsetService.this.sendStatus(); // TODO
+          break;
+        }
+
+        case BluetoothHeadset.ACTION_CONNECTION_STATE_CHANGED: {
+          final int state = intent.getIntExtra(BluetoothProfile.EXTRA_STATE, -1);
+          if (Log.isLoggable(TAG, Log.DEBUG)) {
+            final BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+            Log.d(TAG, String.format("Received broadcast: %s state=%d device=%s", intent.toString(), state, device.toString()));
+          }
+          HeadsetService.this.status.hasWirelessHeadset = (state == BluetoothProfile.STATE_CONNECTED);
+          //HeadsetService.this.sendStatus(); // TODO
+          if (HeadsetService.this.audioManager != null) {
+            if (HeadsetService.this.status.hasWirelessHeadset) {
+              HeadsetService.this.audioManager.startBluetoothSco();
+            }
+            else {
+              HeadsetService.this.audioManager.stopBluetoothSco();
+            }
+          }
+          break;
+        }
+
+        case AudioManager.ACTION_SCO_AUDIO_STATE_UPDATED: {
+          final int state = intent.getIntExtra(AudioManager.EXTRA_SCO_AUDIO_STATE, AudioManager.SCO_AUDIO_STATE_ERROR);
+          if (Log.isLoggable(TAG, Log.DEBUG)) {
+            Log.d(TAG, String.format("Received broadcast: %s state=%d", intent.toString(), state));
+          }
+          switch (state) {
+            // Audio channel is being established
+            case AudioManager.SCO_AUDIO_STATE_CONNECTING: {
+              // nothing to do just yet
+              break;
+            }
+            // Audio channel is established
+            case AudioManager.SCO_AUDIO_STATE_CONNECTED: {
+              if (HeadsetService.this.recordingThread == null) {
+                HeadsetService.this.recordingThread = new AudioRecorderThread();
+                HeadsetService.this.recordingThread.start();
+              }
+              break;
+            }
+            // Audio channel is not established
+            case AudioManager.SCO_AUDIO_STATE_DISCONNECTED: {
+              if (HeadsetService.this.recordingThread != null) {
+                HeadsetService.this.recordingThread.interrupt();
+                HeadsetService.this.recordingThread = null;
+              }
+              break;
+            }
+            // An error trying to obtain the state
+            case AudioManager.SCO_AUDIO_STATE_ERROR: {
+              // should be unreachable
+              break;
+            }
+          }
+          break;
+        }
+
+        default: break; // ignore UFOs
       }
     }
   };
