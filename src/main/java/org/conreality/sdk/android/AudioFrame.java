0939a9bdec005e54e4c81f9465d189a366dbb349
Arto Bendiken <arto@conreality.org>
2019-08-16 01:19:34 +0300

    Implemented AudioFrame#toByte{Array,Buffer}().

--- src/main/java/org/conreality/sdk/android/AudioFrame.java
+++ src/main/java/org/conreality/sdk/android/AudioFrame.java
@@ -3,13 +3,27 @@
 package org.conreality.sdk.android;
 
 import androidx.annotation.NonNull;
+import java.nio.ByteBuffer;
 import java.util.Objects;
 
 /** AudioFrame */
 public class AudioFrame {
   public final @NonNull byte[] data;
 
+  public AudioFrame(final @NonNull ByteBuffer data) {
+    this.data = new byte[data.remaining()];
+    data.get(this.data);
+  }
+
   public AudioFrame(final @NonNull byte[] data) {
     this.data = Objects.requireNonNull(data);
+  }
+
+  public @NonNull byte[] toByteArray() {
+    return this.data.clone();
+  }
+
+  public @NonNull ByteBuffer toByteBuffer() {
+    return ByteBuffer.wrap(this.data.clone());
   }
 }
